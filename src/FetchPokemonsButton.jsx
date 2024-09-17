aec6d894ead2b3d6825b19f8124506269d5e7de2
David Tang <687449+iamdtang@users.noreply.github.com>
2024-09-16 19:35:46 -0700

    Replacing $.ajax with fetch

--- src/FetchPokemonsButton.jsx
+++ src/FetchPokemonsButton.jsx
@@ -1,18 +1,17 @@
-import $ from "jquery";
-
 export default function FetchPokemonsButton(props) {
   return (
     <button
       type="button"
       onClick={() => {
-        const promise = $.ajax({
-          type: "GET",
-          url: "https://pokeapi.co/api/v2/pokemon",
-        });
+        const promise = fetch("https://pokeapi.co/api/v2/pokemon");
 
-        promise.then((response) => {
-          props.onFetch(response.results);
-        });
+        promise
+          .then((response) => {
+            return response.json();
+          })
+          .then((response) => {
+            props.onFetch(response.results);
+          });
       }}
     >
       {props.text}
