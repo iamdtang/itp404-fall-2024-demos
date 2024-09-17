948df9de6fd8343b25437a45c493002a7f9f9b1a
David Tang <687449+iamdtang@users.noreply.github.com>
2024-09-16 19:22:58 -0700

    End of Week 4 demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import $ from "jquery";
+import { useState } from "react";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          const promise = $.ajax({
+            type: "GET",
+            url: "https://pokeapi.co/api/v2/pokemon",
+          });
+
+          promise.then((response) => {
+            setPokemons(response.results);
+          });
+        }}
+      >
+        Show Pokemon
+      </button>
+
+      {pokemons.length > 0 && (
+        <ul>
+          {pokemons.map((pokemon) => {
+            return <li key={pokemon.name}>{pokemon.name}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 }
